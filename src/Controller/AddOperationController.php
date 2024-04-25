4faa68396a8ee399667775184bb19f869461424e
Delhumeur <126495668+Delhumeur@users.noreply.github.com>
2024-04-25 13:36:06 +0200

    fix pb class Opération et client

--- src/Controller/AddOperationController.php
+++ src/Controller/AddOperationController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use App\Entity\Operation;
+use App\Form\AddOperationFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,15 +16,29 @@ class AddOperationController extends AbstractController
     #[Route('/add/operation', name: 'app_add_operation')]
     public function index(Operation $operation, Client $client, Request $request, EntityManagerInterface $entityManager) : Response
     {
+        // Créez une instance de l'entité Operation
+
         $operation = new Operation();
-        $form = $this->createForm(AddOperationFormType::class, $client);
+
+         // Créez un formulaire en utilisant l'entité Operation
+
+        $form = $this->createForm(AddOperationFormType::class, $operation);
+
+        // Gérez la soumission du formulaire
+
         $form -> handleRequest($request);
+
+        // Je check si le formulaire est soumis et valide
+
         if ($form->isSubmitted() && $form->isValid()){
+
             $entityManager->persist($operation);
             $entityManager->flush();
 
             return $this->redirectToRoute('/templates/home/index.html.twig');
         }
+
+        // J'affiche le form dans ma vue
 
         return $this->render('add_operation/index.html.twig', [
             'form' => $form->createView(),
