4503b773d2e281ba42617750f75d70f3cdfd6e72
Franck Lamy <157897618+Franck-Lamy-Dev-Web@users.noreply.github.com>
2024-04-30 16:13:25 +0200

    modif tuiles haut

--- src/Controller/SalesController.php
+++ src/Controller/SalesController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use App\Repository\OperationRepository;
+use DateTime;
 
 class SalesController extends AbstractController
 {
@@ -17,11 +18,27 @@ class SalesController extends AbstractController
         // Initialisation des variables
         $currentYearSales = 0;
         $percentageCurrentSales = 0;
+        $monthSales = 0;
+        $percentageMonthSales = 0;
+        $monthSales = 0;
 
         // Calcul des ventes de l'année en cours
         foreach ($operations as $operation) {
             $currentYearSales += $operation->getTarif();
         }
+
+        //Definition de la date du mois en cours
+        $currentMonth = (new DateTime())-> format('m');
+
+        //Calcul des ventes du mois en cours
+        foreach ($operations as $operation) {
+            if ($operation->getDateCreation()->format('m') === $currentMonth) {
+                $monthSales += $operation->getTarif();
+            }
+        }
+
+        //Calcul du pourcentage des ventes du mois en cours
+        $percentageMonthSales = number_format((($monthSales * 100) / 24100), 1);
         
         // Calcul du pourcentage de ventes par rapport à l'objectif
         $percentageCurrentSales = number_format((($currentYearSales * 100) / 120500), 1);
@@ -30,7 +47,6 @@ class SalesController extends AbstractController
         $percentageObjectif = 5.4;
         $objectif = 120500;
         $monthSales = 6250;
-        $percentageMonthSales = number_format((($monthSales * 100) / 24100), 1);
 
         // Rendu de la vue avec les données
         return $this->render('sales/stats.html.twig', [
