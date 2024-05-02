0b71d3a5b0def9c385d67a6a6f6a55bdec581adc
joJosh59 <joshua97@hotmail.fr>
2024-05-02 15:33:31 +0200

    Regroupement app partie client dans dossier home, ajout lien sur le footer et ajout page mentions legale et CGU/CGV

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,22 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    #[Route('/tarif', name: 'app_tarif')]
+    public function tarif(): Response
+    {
+        return $this->render('home/tarif.html.twig');
+    }
+
+    #[Route('/mentionL', name: 'app_mentionL')]
+    public function mentionL(): Response
+    {
+        return $this->render('home/mentionL.html.twig');
+    }
+
+    #[Route('/CG', name: 'app_cguCgv')]
+    public function cguCgv(): Response
+    {
+        return $this->render('home/cguCgv.html.twig');
+    }
 }
