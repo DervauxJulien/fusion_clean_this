73d2a725a0ff20f8838926db4d2901ac0aee3d2e
Delhumeur <integralille@outlook.fr>
2024-05-02 16:05:31 +0200

    update accordion btn

--- src/Form/AddOperationFormType.php
+++ src/Form/AddOperationFormType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+    
 class AddOperationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
