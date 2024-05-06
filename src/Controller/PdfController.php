2bc4a94e240f5cd6150230362484348b251e0acc
Franck Lamy <francklamymagie@gmail.com>
2024-05-06 09:57:33 +0200

    Ajout telecharger facture pdf

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -21,38 +21,56 @@ class PdfController extends AbstractController
     #[Route('/pdf', name: 'app_pdf')]
     public function index(): Response
     {
+        // Récupérer toutes les opérations
+    $operations = $this->operationRepository->findAll();
+
         return $this->render('pdf/index.html.twig', [
             'controller_name' => 'PdfController',
+            'operations'=>$operations,
         ]);
     }
 
-    #[Route('/generate-pdf/{id}', name: 'app_generate_pdf')]
-    public function generatePdfAction($id): Response
-    {
-        // Récupérer l'opération depuis la base de données
-        $operation = $this->operationRepository->find($id);
+#[Route('/generate-pdf/{id}', name: 'app_generate_pdf')]
+public function generatePdfAction($id): Response
+{
+    // Récupérer l'opération depuis la base de données
+    $operation = $this->operationRepository->find($id);
 
-        // Créer une instance de Dompdf avec les options
-        $pdfOptions = new Options();
-        $pdfOptions->set('defaultFont', 'Arial');
-
-        // Instancier Dompdf avec les options
-        $dompdf = new Dompdf($pdfOptions);
-
-        // Générer le contenu HTML de la facture
-        $html = $this->renderView('pdf/invoice.html.twig', [
-            'operation' => $operation,
-        ]);
-
-        // Charger le contenu HTML dans Dompdf
-        $dompdf->loadHtml($html);
-
-        // Rendre le contenu en PDF
-        $dompdf->render();
-
-        // Envoyer le PDF en réponse
-        return new Response($dompdf->output(), Response::HTTP_OK, [
-            'Content-Type' => 'application/pdf',
-        ]);
+    // Vérifier si l'opération existe
+    if (!$operation) {
+        throw $this->createNotFoundException('Opération non trouvée');
     }
+
+    // Créer une instance de Dompdf avec les options
+    $pdfOptions = new Options();
+
+    // Instancier Dompdf avec les options
+    $dompdf = new Dompdf($pdfOptions);
+
+    // Générer le contenu HTML de la facture
+    $html = $this->renderView('pdf/invoice.html.twig', [
+        'operation' => $operation,
+    ]);
+
+      // Récupérer le nom du client
+      $clientName = $operation->getClient()->getNom();
+
+    // Charger le contenu HTML dans Dompdf
+    $dompdf->loadHtml($html);
+
+    // Rendre le contenu en PDF
+    $dompdf->render();
+
+    // Remplacer les caractères spéciaux dans le nom du client
+     $clientName = str_replace([' ', '/'], '_', $clientName);
+
+    // Construire le nom du fichier PDF avec le nom du client
+     $fileName = $clientName . '_facture.pdf';
+
+    // Envoyer le PDF en réponse
+    return new Response($dompdf->output(), Response::HTTP_OK, [
+        'Content-Type' => 'application/pdf',
+        'Content-Disposition' => 'attachment; filename="Facture_CleanThis_' . $clientName . '.pdf"',
+    ]);
+}
 }
