187034d358554185d7af393de20a792735188001
Stan <154512651+StanislasCasamajor@users.noreply.github.com>
2024-05-14 09:36:15 +0200

    Avancement de la page demande opération, formulaire ok, manque google lens

--- src/Controller/DemandeOperationController.php
+++ src/Controller/DemandeOperationController.php
@@ -8,6 +8,7 @@ use App\Entity\Operation;
 use App\Form\AdresseType;
 use App\Form\ClientType;
 use App\Form\OperationType;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -41,24 +42,34 @@ class DemandeOperationController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            // $data = $form->getData();
+            // J'envoi les données renseignées par le formulaire en base de donnée, comme c'est un formulaire imbriqué il faut respecter l'ordre des flush pour éviter les erreurs.
 
-            $em->persist($client);
-            $em->flush();
-
-            $em->persist($client);
+            // Flush de l'adresse en premier car pas de clé étrangère.
             $em->persist($adresse);
             $em->flush();
 
+            // On renseigne l'adresse_id depuis l'entitée Adresse vers l'entité client sinon celle-ci sera définie comme nulle.
+
+            $client->setAdresse($adresse);
             $em->persist($client);
-            $em->persist($adresse);
+            $em->flush();
+
+            $operation->setStatus('à faire'); 
+            $operation->setDateDemande(new \DateTime());
+            $operation->setAdresse($adresse);
+            $operation->setClient($client);
             $em->persist($operation);
             $em->flush();
 
-            return $this->redirectToRoute('validation');
+            return $this->redirectToRoute('app_validation');
         }
         return $this->render('demande_operation/index.html.twig', [
             "createForm" => $form->createView()
         ]);
+    }
+
+    #[Route('/validation', name: 'app_validation')]
+    public function validation(){
+        return $this->render('demande_operation/validation.html.twig');
     }
 }
