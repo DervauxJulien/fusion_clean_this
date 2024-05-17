77a082e143877acb3b794ae6adb955577170813e
Franck Lamy <francklamymagie@gmail.com>
2024-05-17 09:26:06 +0200

    tableau op terminer par ordre alphabetique

--- src/Controller/TableController.php
+++ src/Controller/TableController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
@@ -11,40 +12,39 @@ use App\Repository\OperationRepository;
 class TableController extends AbstractController
 {
     #[Route('/table', name: 'table')]
-    public function tableIndex(UserRepository $userRepository, OperationRepository $operationRepository): Response
+    public function tableIndex(Request $request, UserRepository $userRepository, OperationRepository $operationRepository): Response
     {
-
-        $objectiveUser = 27560;
-
-        // Récupération de l'objectif depuis le localStorage
-        // $objectif = $request->getSession()->get('objectif');
-
-        // Si l'objectif n'est pas encore défini dans le localStorage, on utilise la valeur par défaut
-        // if (!$objectif) {
-        //     $objectif = 220500;
-        // }
-
-
-
+        
+        // Récupération de l'objectif depuis la session
+        $objectif = $request->getSession()->get('objectif');
+        //Calcul de l'objectif par user
+        $objectiveUser = number_format(($objectif/8),0) ;
+        // $objectiveUser = 27500;
+        
+        // Si l'objectif n'est pas encore défini dans la session, on utilise la valeur par défaut
+        if (!$objectif) {
+            $objectif = 220500;
+            $objectiveUser = 27560;
+        }
+        
 
         // Récupération des opérations depuis le repository
         $operations = $operationRepository->findAll();
+        
         // Initialisation d'un tableau pour stocker les statuts des opérations
-        $status = [];
+        $statuts = [];
+        
         // Boucle sur chaque opération pour obtenir son statut
         foreach ($operations as $operation) {
-            $status[] = $operation->getStatus(); // Stocker chaque statut dans le tableau $statuts
+            $statuts[] = $operation->getStatus(); // Stocker chaque statut dans le tableau $statuts
         }
 
-       
-
         return $this->render('table/index.html.twig', [
-            // 'products' => $data,
             'objectiveUser' => $objectiveUser,
             'users' => $userRepository->findAll(),
             'operations' => $operations,
-            'status' => $status,
+            'statuts' => $statuts,
+            'objectif' => $objectif,
         ]);
-
     }
 }
